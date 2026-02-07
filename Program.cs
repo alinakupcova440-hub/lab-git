d2c6a8044fbdd8bea6f64cb60821f45d267e6066
Alina Kupsova <alinakupcova440@gmail.com>
2026-02-07 19:40:39 +0600

    Добавлена новая строка в Program.cs

--- Program.cs
+++ Program.cs
@@ -4,4 +4,4 @@ Console.WriteLine("Группа: ДИН-409");
 
 Console.Write("Введите число: ");
 int num = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine($"Квадрат числа: {num * num}");
+Console.WriteLine($"Квадрат числа: {num * num}");// Добавлена новая строка
