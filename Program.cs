1078cbc0911141e504fbed70be85e07b09fdd594
Alina Kupsova <alinakupcova440@gmail.com>
2026-02-07 19:30:38 +0600

    Добавлен проект консольного приложения

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Лабораторная работа по Git");
+Console.WriteLine("Студент: Купцова Алина");
+Console.WriteLine("Группа: ДИН-409");
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Квадрат числа: {num * num}");
