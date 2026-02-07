bf16369ce255e5b52abaf0b429e1aa0072927e73
Alina Kupsova <alinakupcova440@gmail.com>
2026-02-07 20:44:02 +0600

    Добавлена новая строка в ветке feature

--- Program.cs
+++ Program.cs
@@ -5,3 +5,4 @@ Console.WriteLine("Группа: ДИН-409");
 Console.Write("Введите число: ");
 int num = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine($"Квадрат числа: {num * num}");// Добавлена новая строка
+Console.WriteLine("Новая строка из ветки feature");
